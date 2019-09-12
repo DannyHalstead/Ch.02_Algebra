1fe16d725dfd9775936ef86bf3607bd36a96c6a0
halsteaddan <halsteaddan@urbandaleschools.com>
2019-09-12 13:36:42 -0500

    Fixed

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,14 +1,9 @@
 '''
 Sign your name: Danny Halstead
-
 1.) How do you enter a single line comment in a program?
 One of these things #
-
-
-
-2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
+2.) Enter a=2 and b=5 in the Python Console window and then all of the following.
 What are the outputs?
-
 b/a                 =2.5
 b//a                =2
 b**a                =25
@@ -18,38 +13,23 @@ type(42)            =class 'int'
 type(42.0)          =class 'float'
 type("C3PO")        =class 'str'
 type(True)          =class 'bool'
-
-
-
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
 into the Python Console Window?
-
 a=2
 a*=10
 a/=2
 a+=12
 a-=7
-
 a             =15
 type(a)       =class 'float'
-
-
-
 4.) Fix the mistake in the following code.
 x,y=(4,5)
 a=3*(x + y)    #Parenthisis does not automatically multiply
 a
 #a would be 27
-
-
-
 5.) Fix the code so it will calculate the average.
 x,y,z =(3,4,5)
 ave=(x+y+z)/3
 ave
 #ave would be 4.0
-
-
-
-
 '''
